438e7e98e9fb69e3725172e774ff41556bb7361f
lawMOB <83550208+LawMOB@users.noreply.github.com>
2026-09-22 04:53:03 +0700

    Add MobileGL renderer definitions and check function
    Added definitions for MobileGL and MobileGL-GLES renderers, and implemented a function to check if a renderer is MobileGL.
    
    Signed-off-by: lawMOB <83550208+LawMOB@users.noreply.github.com>

--- Natives/utils.h
+++ Natives/utils.h
@@ -3,6 +3,7 @@
 #import <UIKit/UIKit.h>
 
 #include <stdbool.h>
+#include <string.h>
 #include "environ.h"
 #include "jni.h"
 
@@ -40,6 +41,13 @@
 #define RENDERER_NAME_LTW "libltw.dylib"
 #define RENDERER_NAME_VK_ZINK "libOSMesa.8.dylib"
 #define RENDERER_NAME_VULKAN "libMoltenVK.dylib"
+#define RENDERER_NAME_MOBILEGL "libMobileGL.dylib"
+#define RENDERER_NAME_MOBILEGL_GLES "libMobileGL-gles.dylib"
+
+static inline bool isMobileGLRenderer(const char *renderer) {
+    return renderer && (!strcmp(renderer, RENDERER_NAME_MOBILEGL) ||
+                        !strcmp(renderer, RENDERER_NAME_MOBILEGL_GLES));
+}
 
 #define SPECIALBTN_KEYBOARD -1
 #define SPECIALBTN_TOGGLECTRL -2
